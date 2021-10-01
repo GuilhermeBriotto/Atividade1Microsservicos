4bd5830be9312518c1ddab2573d3d3ac6c95d6df
GuilhermeBriotto <61739330+GuilhermeBriotto@users.noreply.github.com>
2021-10-01 20:50:54 -0300

    Add files via upload

---
+++ nac-nodejs-main/src/repositories/user-repository.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+exports.get = async () => {
+  const result = await User.find();
+  return result;
+};
+
+exports.create = async (data) => {
+  let user = User(data);
+  await user.save();
+};
+
+exports.getById = async (id) => {
+  const result = await User.findById(id);
+  return result;
+};
+
+exports.udpate = async (id, data) => {
+  await User.findByIdAndUpdate(id, {
+    $set: {
+      name: data.name,
+      address: data.address,
+      age: data.age,
+    },
+  });
+};
+
+exports.delete = async (id) => {
+  await User.findByIdAndDelete(id);
+};
+
